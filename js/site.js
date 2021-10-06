a51f12a1d83b278f95f425ff61276526f85a709c
WILLIAMTHEWEST1 <90918613+WILLIAMTHEWEST1@users.noreply.github.com>
2021-10-06 13:12:16 -0500

    initial changes

--- js/site.js
+++ js/site.js
@@ -1,18 +1,50 @@
-//displays a message to the user
-function displayMessage() {
+//Get starting values from the screen
+//Controlling function
+function getUserString() {
+    //get the user's input
+    let userstring = document.getElementById("userString")
+    //reverse the user's input- separate function
+    let revString = reverseString(userString)
 
-    let msg = "";
+    //compare original input and reversed input - separate function
+    let result = compareStrings(userString, revString)
 
-    msg = document.getElementById("message").value;
+    //output results- separate function
+    displayString(result);
+};
 
-    alert(msg);
-}
+    //This is REWIND'S JS 
+    function reverseString(userString) {
+        let startValue = userString.length - 1;
+        let endValue = 0;
+        let revString = "";
 
-function displayMessage2() {
 
-let msg = "";
+        for (let index = startValue; index >= endValue; index--) {
+            revString += userString[index];
+        };
 
-msg = document.getElementById("message").value;
+        return revString
 
-Swal.fire(msg)
-}
+
+    };
+    //This we create
+    //A != a
+    //Normalize the case
+    //Extra credit- "A man, a plan, a canal; Panama" - is a palinddrome
+    //RedEx is used for this- Google it.
+    function compareStrings(userString, revString) {
+        let user = userString.toLowerCase()
+        let rev = revString.toLowerCase()
+
+    };
+
+    //We want to output three things
+    //1. The original input.
+    //2.The reversed input.
+    //3. Was it a palindrome?
+    function displayString(revString) {
+        let output = document.getElementById("results");
+        output.innerHTML = revString;
+
+    };
